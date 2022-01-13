fc8c2c9590179e9703b629bc0e306077310f6005
Giuseppe Caponetto <ggcaponetto@gmail.com>
2022-01-13 14:39:25 +0100

    add batch qr code generation from csv

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var QRCode = require('qrcode')
+const csv = require('csv-parser')
+const fs = require('fs');
+const path = require("path");
+
+const rows = [];
+(async () => {
+  return await new Promise((res, rej) => {
+    fs.createReadStream(path.resolve(`${__dirname}/../input/input.csv`))
+      .pipe(csv())
+      .on('data', (data) => rows.push(data))
+      .on('end', () => {
+        console.log("input:\n", rows);
+        for(let i = 0; i < rows.length; i++){
+          let row = rows[i];
+          QRCode.toString(row.URL, {type:'terminal'}, function (err, url) {
+            console.log(`converted text ${i+1} (${row.URL}) into qr code: \n${url}`)
+          })
+        }
+        res("qr codes generated");
+      });
+  });
+})()
